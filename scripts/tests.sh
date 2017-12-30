6a84f2218416b69cba1731fbef21fe4292f5d55b
Joe Mewes <joe@4alldigital.com>
2017-12-30 18:44:15 +0000

    #164

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -18,7 +18,7 @@ EOF
 echo -e "${GREEN}DEMO TEST${NC}"
 cd /Users/joe/dev/playground && \
 rm -rf testdev && \
-./bin/drudock env:init testdev --type D8 --dist Development --src New --apphost drudock.dev --services "PHP,NGINX,MYSQL" && \
+./bin/drudock env:init testdev --type D8 --dist Development --src New --apphost drudock.localhost --services "PHP,NGINX,MYSQL" && \
 cd testdev && \
 ./bin/drudock build:init && \
 curl -O http://localhost
