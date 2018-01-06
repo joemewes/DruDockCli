4bd150b23ee43777b7d314324364aef6f212f97d
Joe Mewes <joe@4alldigital.com>
2018-01-06 02:05:25 +0000

    #159
    cleaned up mydql dump backups

--- src/Command/Mysql/MysqlExportCommand.php
+++ src/Command/Mysql/MysqlExportCommand.php
@@ -23,6 +23,15 @@ use Docker\Drupal\Extension\ApplicationContainerExtension;
  */
 class MysqlExportCommand extends Command {
 
+  /**
+   * Define constants
+   */
+
+  // Config constants.
+  const APP_NAME = 'appname';
+
+  const APP_TYPE = 'apptype';
+
   protected function configure() {
     $this
       ->setName('mysql:export')
@@ -39,7 +48,8 @@ class MysqlExportCommand extends Command {
     $io->section('MYSQL ::: dump/export database');
 
     if ($config = $application->getAppConfig($io)) {
-      $appname = $config['appname'];
+      $appname = $config[self::APP_NAME];
+      $type = $config[self::APP_TYPE];
     }
     else {
       $appname = 'app';
@@ -56,8 +66,114 @@ class MysqlExportCommand extends Command {
       $savepath = $helper->ask($input, $output, $question);
     }
 
+    $mysqldump = "mysqldump -u drudock -pMYSQLPASS drudock_db";
+
+    if (isset($type) && $type == 'D7') {
+      $mysqldump = "(mysqldump  -u drudock -pMYSQLPASS drudock_db \
+                    --ignore-table=drudock_db.cache \
+                    --ignore-table=drudock_db.cache_admin_menu \
+                    --ignore-table=drudock_db.cache_block \
+                    --ignore-table=drudock_db.cache_bootstrap \
+                    --ignore-table=drudock_db.cache_entity_comment \
+                    --ignore-table=drudock_db.cache_entity_fieldable_panels_pane \
+                    --ignore-table=drudock_db.cache_entity_file \
+                    --ignore-table=drudock_db.cache_entity_node \
+                    --ignore-table=drudock_db.cache_entity_paragraphs_item \
+                    --ignore-table=drudock_db.cache_entity_taxonomy_term \
+                    --ignore-table=drudock_db.cache_entity_taxonomy_vocabulary \
+                    --ignore-table=drudock_db.cache_entity_user \
+                    --ignore-table=drudock_db.cache_entityconnect \
+                    --ignore-table=drudock_db.cache_features \
+                    --ignore-table=drudock_db.cache_feeds_http \
+                    --ignore-table=drudock_db.cache_field \
+                    --ignore-table=drudock_db.cache_filter \
+                    --ignore-table=drudock_db.cache_form \
+                    --ignore-table=drudock_db.cache_image \
+                    --ignore-table=drudock_db.cache_libraries \
+                    --ignore-table=drudock_db.cache_mailchimp \
+                    --ignore-table=drudock_db.cache_menu \
+                    --ignore-table=drudock_db.cache_metatag \
+                    --ignore-table=drudock_db.cache_oembed \
+                    --ignore-table=drudock_db.cache_page \
+                    --ignore-table=drudock_db.cache_panels \
+                    --ignore-table=drudock_db.cache_path \
+                    --ignore-table=drudock_db.cache_rules \
+                    --ignore-table=drudock_db.cache_token \
+                    --ignore-table=drudock_db.cache_update \
+                    --ignore-table=drudock_db.cache_variable \
+                    --ignore-table=drudock_db.cache_views \
+                    --ignore-table=drudock_db.cache_views_data \
+                    --ignore-table=drudock_db.history \
+                    --ignore-table=drudock_db.sessions \
+                    --ignore-table=drudock_db.watchdog &&
+                    mysqldump  -u drudock -pMYSQLPASS drudock_db \
+                    --no-data cache \
+                    --no-data cache_admin_menu \
+                    --no-data cache_block \
+                    --no-data cache_bootstrap \
+                    --no-data cache_entity_comment \
+                    --no-data cache_entity_fieldable_panels_pane \
+                    --no-data cache_entity_file \
+                    --no-data cache_entity_node \
+                    --no-data cache_entity_paragraphs_item \
+                    --no-data cache_entity_taxonomy_term \
+                    --no-data cache_entity_taxonomy_vocabulary \
+                    --no-data cache_entity_user \
+                    --no-data cache_entityconnect \
+                    --no-data cache_features \
+                    --no-data cache_feeds_http \
+                    --no-data cache_field \
+                    --no-data cache_filter \
+                    --no-data cache_form \
+                    --no-data cache_image \
+                    --no-data cache_libraries \
+                    --no-data cache_mailchimp \
+                    --no-data cache_menu \
+                    --no-data cache_metatag \
+                    --no-data cache_oembed \
+                    --no-data cache_page \
+                    --no-data cache_panels \
+                    --no-data cache_path \
+                    --no-data cache_rules \
+                    --no-data cache_token \
+                    --no-data cache_update \
+                    --no-data cache_variable \
+                    --no-data cache_views \
+                    --no-data cache_views_data \
+                    --no-data history \
+                    --no-data sessions \
+                    --no-data watchdog \
+                    )";
+    }
+
+    if (isset($type) && $type == 'D8') {
+      $mysqldump = "(mysqldump  -u drudock -pMYSQLPASS drudock_db \
+                    --ignore-table=drudock_db.cache_bootstrap \
+                    --ignore-table=drudock_db.cache_config \
+                    --ignore-table=drudock_db.cache_container \
+                    --ignore-table=drudock_db.cache_data \
+                    --ignore-table=drudock_db.cache_default \
+                    --ignore-table=drudock_db.cache_discovery \
+                    --ignore-table=drudock_db.cache_entity \
+                    --ignore-table=drudock_db.cache_menu \
+                    --ignore-table=drudock_db.cache_page \
+                    --ignore-table=drudock_db.cachetags && \
+                    mysqldump  -u drudock -pMYSQLPASS drudock_db \
+                    --no-data cache_bootstrap \
+                    --no-data cache_config \
+                    --no-data cache_container \
+                    --no-data cache_data \
+                    --no-data cache_default \
+                    --no-data cache_discovery \
+                    --no-data cache_entity \
+                    --no-data cache_menu \
+                    --no-data cache_page \
+                    --no-data cachetags \
+                    )";
+    }
+
     if ($container_application->checkForAppContainers($appname, $io) && isset($savepath)) {
-      $command = $container_application->getComposePath($appname, $io) . 'exec -T mysql mysqldump -u drudock -pMYSQLPASS drudock_db > ' . $savepath;
+      $command = $container_application->getComposePath($appname, $io) . 'exec -T mysql bash -c "' . $mysqldump . '" > ' . $savepath;
       $application->runcommand($command, $io);
     }
   }
